d18abdb78c09bbca50c5e3d55f283021fa2e011a
Syed Mustafa Ahmed <77160234+syedmustafa026@users.noreply.github.com>
2021-09-28 03:11:10 -0700

    Just copy and Paste use Public and Private routing

--- router.js
+++ router.js
@@ -1,12 +1,53 @@
-// Install npm react-router-dom first...
-
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect, useLocation } from "react-router-dom";
 import Login from '../containers/Login';
 import Signup from "../containers/Signup";
 import Profile from '../containers/Profile';
+import Chat from '../containers/Chat'
 import { onAuthStateChanged, auth } from './Firebase';
 import { useEffect } from 'react';
 import { useState } from 'react';
+
+
+
+function PrivateRoute({ component: Component, auth, ...rest }) {
+    return (
+        <Route
+            {...rest}
+            component={({ location }) =>
+                auth ? (
+                    <Component />
+                ) : (
+                    <Redirect
+                        to={{
+                            pathname: "/",
+                            state: {
+                                from: location.pathname,
+                            },
+                        }}
+                    />
+                )
+            }
+        />
+    );
+}
+
+function PublicRoute({ component: Component, auth, ...rest }) {
+    const location = useLocation();
+    return (
+        <Route
+            {...rest}
+            component={() =>
+                !auth ? (
+                    <Component />
+                ) : (
+                    <Redirect
+                        to={location.state.from ? location.state.from : "/profile"}
+                    />
+                )
+            }
+        />
+    );
+}
 
 
 function AppRouter() {
@@ -15,25 +56,19 @@ function AppRouter() {
         onAuthStateChanged(auth, (user) => {
             if (user) {
                 setIsAuth(true)
-            }
-            else{
+            } else {
                 setIsAuth(false)
             }
         })
     }, [])
     return (
         <Router>
-            {isAuth ?
-                <Switch>
-                    <Route exact path="/" component={Profile} />
-                </Switch>
-                :
-                <Switch>
-                    <Route exact path="/" component={Login} />
-                    <Route exact path="/signup" component={Signup} />
-                    
-                </Switch>
-            }
+            <Switch>
+                <PublicRoute auth={isAuth} exact path="/" component={Login} />
+                <PublicRoute auth={isAuth} exact path="/signup" component={Signup} />
+                <PrivateRoute auth={isAuth} exact path="/profile" component={Profile} />
+                <PrivateRoute auth={isAuth} exact path="/chat" component={Chat} />
+            </Switch>
         </Router>
     )
 }
